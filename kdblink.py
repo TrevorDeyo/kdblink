f04075d2d6a2102a17ee7fd81c2bf615676af284
Deyo <36089323+TrevorDeyo@users.noreply.github.com>
2024-05-09 01:26:23 -0500

    Update kdblink.py

--- kdblink.py
+++ kdblink.py
@@ -1,3 +1,4 @@
+import os
 import asyncio
 from aiohttp import ClientSession
 from blinkpy.blinkpy import Blink
@@ -6,7 +7,7 @@ from blinkpy.auth import Auth
 async def start():
     blink = Blink(session=ClientSession())
     # Can set no_prompt when initializing auth handler
-    auth = Auth({"username": <username>, "password": <your password>}, no_prompt=True)
+    auth = Auth({"username": os.environ.get('username'), "password": os.environ.get('password')}, no_prompt=True)
     blink.auth = auth
     await blink.start()
     return blink
