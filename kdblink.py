8ce085e2b7fba9d9799c5efceaa4982ef1ca239c
Deyo <36089323+TrevorDeyo@users.noreply.github.com>
2024-05-09 02:43:10 -0500

    finally made some progress
    I was under the impression for a whole 2 hours that it wanted a folder path to create and write the creds to but in reality it wanted a json file path rolf im tired goodnight cruel world

--- kdblink.py
+++ kdblink.py
@@ -1,15 +1,20 @@
-import os
 import asyncio
 from aiohttp import ClientSession
 from blinkpy.blinkpy import Blink
 from blinkpy.auth import Auth
+from blinkpy.helpers.util import json_load
 
 async def start():
-    blink = Blink(session=ClientSession())
-    # Can set no_prompt when initializing auth handler
-    auth = Auth({"username": os.environ.get('username'), "password": os.environ.get('password')}, no_prompt=True)
+    blink = Blink()
+    auth = Auth(await json_load(r"C:\Code\kdblink\creditials.json"))
+    await blink.save("C:\Code\kdblink\creditials.json")
     blink.auth = auth
     await blink.start()
+    for name, camera in blink.cameras.items():
+        print(name)                   # Name of the camera
+        print(camera.attributes)      # Print available attributes of camera
     return blink
+
+
 
 blink = asyncio.run(start())
